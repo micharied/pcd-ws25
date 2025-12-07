efd72c5e5b20e00df6592c2b130b02458be866e1
Philipp Schuster <philipp.schuster@uni-tuebingen.de>
2025-12-07 11:04:36 +0100

    Update slides 06

--- 05_Channels/exercise/primes/main.go
+++ 05_Channels/exercise/primes/main.go
@@ -1,18 +1,46 @@
 package main
 
-// import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func generate(n int, channel chan<- int) {
-	panic("TODO")
+	for i := 2; i <= n; i++ {
+		channel <- i
+	}
+	close(channel)
 }
 
 func filter(prime int, input <-chan int, output chan<- int) {
-	panic("TODO")
+	for n := range input {
+		if n%prime != 0 {
+			output <- n
+		}
+	}
+	close(output)
 }
 
 func main() {
-	n := 100
+	n := 1000000
 
-	panic("TODO")
+	start := time.Now()
 
+	input := make(chan int)
+	go generate(n, input)
+
+	for {
+		prime, ok := <-input
+		if !ok {
+			break 
+		}
+
+		fmt.Println(prime)
+
+		next := make(chan int)
+		go filter(prime, input, next)
+		input = next 
+	}
+
+	fmt.Printf("Sieved up to %d in %s\n", n, time.Since(start))
 }
